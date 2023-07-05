07354b693549e70e36735504052f35d856fdf5f8
slothy-bear <seayoung931@gmail.com>
2023-07-05 14:57:38 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,19 +1,3 @@
-const searchEl = document.querySelector('.search');
-const serachInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function(){
-    serachInputEl.focus();
-});
-
-serachInputEl.addEventListener('focus', function(){
-    searchEl.classList.add('focused');
-    serachInputEl.setAttribute('placeholder', '통합검색');
-});
-serachInputEl.addEventListener('blur', function(){
-    searchEl.classList.remove('focused');
-    serachInputEl.setAttribute('placeholder', '');
-});
-
 
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
@@ -169,7 +153,3 @@ spyEls.forEach(function(spyEls){
         .setClassToggle(spyEls,'show')
         .addTo(new ScrollMagic.Controller());
 });
-
-
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear();
